823842a0cbf9e118dab65440d65c9866916d7ff7
Mona Lisa <pterziev@datexcorp.com>
2024-05-01 16:03:40 +0300

    Updated code

--- src/main/java/com/sharedservices/Application.java
+++ src/main/java/com/sharedservices/Application.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-@EnableHystrix
+//@EnableHystrix
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class,
